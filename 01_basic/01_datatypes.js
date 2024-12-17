b8063c7a015e9f2cbb77919c17e53f3782633993
Anurag-methi <162001152+Anurag-methi@users.noreply.github.com>
2024-12-17 17:21:42 +0000

    data types in js

---
+++ 01_basic/01_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+console.log(3+3);
+console.log("a" + "b");
+console.log(typeof 3);
+let name = "anurag"
+console.log(typeof name);
+console.log(typeof null);//object
+console.log(typeof undefined);// undefined
+/*
+ number range => 2 to the power 53
+ bigint => for big numbers
+ boolean => true/false
+ string => "Anurag"
+ symbol => unique
+ null => standalone object
+ undefined => undefined
+ */
+let number = BigInt(36893488147419103232)
+console.log(number);
+let age = null;
+console.log(age);
+let age1 = undefined
+console.log(age1);
+
+
+
+
+
+
+
+
