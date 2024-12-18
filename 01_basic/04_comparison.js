1b8c6671381338aaa545f016b33a2b423190af0d
Anurag-methi <162001152+Anurag-methi@users.noreply.github.com>
2024-12-18 17:58:35 +0000

    comparisons

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,11 @@
+console.log("2" == 2)//true
+console.log("2" >= 1);//true
+console.log("2" >1)// true
+console.log(3 === "3");//false(strict)
+console.log(null == 0)//false
+console.log(null <= 0);//true
+console.log(undefined == 0);// false
+console.log(undefined == NaN)//false
+console.log("3" > "1012")// true
+
+
